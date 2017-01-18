67f0c6b7a2f8fd0619561ae145ae8733220ebacd
tianff <tianff@mapbar.com>
2017-01-18 17:34:31 +0800

    chg gradle配置

--- app/src/main/java/com/example/tianye/jenkinsdemo/MainActivity.java
+++ app/src/main/java/com/example/tianye/jenkinsdemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.tianye.jenkinsdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -9,7 +11,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        printfDeviceInfo();
         //你好
         //第三次
+    }
+
+    private void printfDeviceInfo() {
+        WindowManager wm = (WindowManager)getSystemService(WINDOW_SERVICE);
+        android.view.Display display = wm.getDefaultDisplay();
+        Log.e("Tag",display.toString());
     }
 }
